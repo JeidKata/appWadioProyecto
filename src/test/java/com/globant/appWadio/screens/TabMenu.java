6c1be955e0d9dcda3bdaf5603567a41bb286f8cb
Jeidy Olaya <jeidkata@gmail.com>
2025-06-23 16:32:37 -0500

    Inicializo el PageFactory y el driver. El PageFactory inicializa los elementos ded una Screen en un modo lazy. Me permite usar los Hubs

---
+++ src/test/java/com/globant/appWadio/screens/TabMenu.java
@@ -0,0 +1,4 @@
+package com.globant.appWadio.screens;
+
+public class TabMenu {
+}
--- src/test/java/com/globant/appWadio/utils/BaseScreen.java
+++ src/test/java/com/globant/appWadio/utils/BaseScreen.java
@@ -1,4 +1,14 @@
 package com.globant.appWadio.utils;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class BaseScreen {
+
+    public AppiumDriver appiumDriver;
+
+    public BaseScreen(AppiumDriver appiumDriver){
+        this.appiumDriver = appiumDriver;
+        PageFactory.initElements(appiumDriver, this);
+    }
 }
