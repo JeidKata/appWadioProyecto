bbf4358bee3e29b3bec9cd51a6d751f29152fb87
Jeidy Olaya <jeidkata@gmail.com>
2025-06-23 13:44:30 -0500

    Creo la clase para los test de navegación del menu bar.

--- src/test/java/com/globant/appWadio/tests/MenuTest.java
+++ src/test/java/com/globant/appWadio/tests/MenuTest.java
@@ -1,4 +1,11 @@
 package com.globant.appWadio.tests;
 
-public class MenuScreen {
+import com.globant.appWadio.utils.BaseTest;
+import org.testng.annotations.Test;
+
+public class MenuTest extends BaseTest {
+
+    @Test
+    public void testMenuNavigation() {
+    }
 }
