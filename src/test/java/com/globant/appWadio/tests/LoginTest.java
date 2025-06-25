0a37e12d83444ed462104ac9266fd1cd5b271e79
Jeidy Olaya <jeidkata@gmail.com>
2025-06-24 23:33:49 -0500

    Agrego el flujo de prueba de Login. La prueba falla porque no encuentra el campo donde debe confirmar la contraseña.

--- src/test/java/com/globant/appWadio/tests/LoginTest.java
+++ src/test/java/com/globant/appWadio/tests/LoginTest.java
@@ -1,4 +1,17 @@
 package com.globant.appWadio.tests;
 
-public class LoginTest {
+import com.globant.appWadio.screens.LoginScreen;
+import com.globant.appWadio.screens.TabMenu;
+import com.globant.appWadio.utils.BaseTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void testLogin() {
+        TabMenu tabMenu = new TabMenu(appiumDriver);
+        LoginScreen loginScreen = tabMenu.tapOnLoginTab();
+        loginScreen.tapOnLoginButton();
+        System.out.println("Login test executed successfully.");
+    }
 }
