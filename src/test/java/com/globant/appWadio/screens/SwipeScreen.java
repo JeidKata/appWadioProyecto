32925e1f0da978d39dc77474fbfd335efef3006d
Jeidy Olaya <jeidkata@gmail.com>
2025-06-27 15:36:42 -0500

    Agrego funciones y variables para probar el apartadon swipe.

--- src/test/java/com/globant/appWadio/screens/SwipeScreen.java
+++ src/test/java/com/globant/appWadio/screens/SwipeScreen.java
@@ -2,25 +2,44 @@ package com.globant.appWadio.screens;
 
 import com.globant.appWadio.utils.BaseScreen;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
 
 public class SwipeScreen extends BaseScreen {
+
+    @AndroidFindBy(uiAutomator = "text(\"Swipe horizontal\")")
+    private WebElement lblSwipeHorizontal;
+
+    @AndroidFindBy(uiAutomator = "description(\"card\").instance(0)")
+    private WebElement actualCardElements;
+
+    @AndroidFindBy(uiAutomator = "description(\"card\").instance(1)")
+    private WebElement nextCardElements;
+
+    @AndroidFindBy(accessibility = "card")
+    private WebElement lastCardElement;
+
+    @AndroidFindBy(uiAutomator = "text(\"COMPATIBLE\")")
+    private WebElement lblCompatible;
+
     public SwipeScreen(AppiumDriver driver) {
         super(driver);
     }
 
-    /**
-     * This method is used to swipe left on the screen.
-     */
-    public void swipeLeft() {
-        // Implement swipe left logic here
-        System.out.println("Swiping left on the screen.");
+    public String getLblLastCard() {
+        return lblCompatible.getDomAttribute("text");
     }
 
     /**
-     * This method is used to swipe right on the screen.
+     * This method is used to swipe horizontally.
      */
-    public void swipeRight() {
-        // Implement swipe right logic here
-        System.out.println("Swiping right on the screen.");
+    public void swipeFromRightToLeft() {
+        waitForElementToAppear(lblSwipeHorizontal);
+        if (nextCardElements.isDisplayed() && actualCardElements.isDisplayed()) {
+            swipeHorizontal(nextCardElements, actualCardElements);
+            System.out.println("Swiped from right to left successfully.");
+        }else {
+            System.out.println("No more cards to swipe left.");
+        }
     }
 }
